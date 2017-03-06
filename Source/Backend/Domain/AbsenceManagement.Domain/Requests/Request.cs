0472574fc65c8c228f0dc9c17d90fa336520a662
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-06 17:02:11 +0100

    Added status enum

--- Source/Backend/Domain/AbsenceManagement.Domain/Requests/Request.cs
+++ Source/Backend/Domain/AbsenceManagement.Domain/Requests/Request.cs
@@ -7,6 +7,7 @@ namespace AbsenceManagement.Domain.Requests
     public class Request : DomainEntity<Guid>
     {
         public DateTime? RequestedAt { get; private set; }
+        public Status Status { get; private set; }
 
         public Person Requestor { get; private set; }
         public Person Requestee { get; private set; }
---
+++ Source/Backend/Domain/AbsenceManagement.Domain/Requests/Status.cs
@@ -0,0 +1,13 @@
+﻿namespace AbsenceManagement.Domain.Requests
+{
+    public enum Status
+    {
+        InRequest,
+        Approved,
+        InProcessing_Approved,
+        Rejected,
+        InProcessing_Rejected,
+        Cancelled,
+        InProcessing_Cancelled
+    }
+}
