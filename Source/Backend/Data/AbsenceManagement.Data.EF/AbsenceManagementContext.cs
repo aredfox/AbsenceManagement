001a55bd7a2da4e975c46ac954cd316237e651f3
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-06 17:09:37 +0100

    Added DbContext and DbSets for Person and Request

---
+++ Source/Backend/Data/AbsenceManagement.Data.EF/AbsenceManagementContext.cs
@@ -0,0 +1,18 @@
+﻿using AbsenceManagement.Domain.People;
+using AbsenceManagement.Domain.Requests;
+using System.Data.Entity;
+
+namespace AbsenceManagement.Data.EF
+{
+    public class AbsenceManagementContext : DbContext
+    {
+        public IDbSet<Person> People { get; set; }
+        public IDbSet<Request> Requests { get; set; }
+
+        public AbsenceManagementContext(string connectionString)
+            : base(connectionString) {
+        }
+        public AbsenceManagementContext() {
+        }
+    }
+}
