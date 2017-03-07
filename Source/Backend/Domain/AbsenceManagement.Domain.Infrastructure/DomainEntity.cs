6ef554674b83b47f6b13571026a741cc1fc52440
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-07 01:02:16 +0100

    Guids now work, generated on DB-level.

--- Source/Backend/Domain/AbsenceManagement.Domain.Infrastructure/DomainEntity.cs
+++ Source/Backend/Domain/AbsenceManagement.Domain.Infrastructure/DomainEntity.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AbsenceManagement.Domain.Infrastructure
 {
     public abstract class DomainEntity<TKey>
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public TKey Id { get; private set; }
 
         protected DomainEntity() { }
