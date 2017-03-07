da03fcbeba1088a61d58728d4df37dd27b6df509
yves.schelpe@kdg.be <yves.schelpe@kdg.be>
2017-03-07 11:41:18 +0100

    Changed seeding logic for SystemUser.

--- Source/Backend/Data/AbsenceManagement.Data.EF/Migrations/Configuration.cs
+++ Source/Backend/Data/AbsenceManagement.Data.EF/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace AbsenceManagement.Data.EF.Migrations
     using Domain.People;
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AbsenceManagementContext>
     {
@@ -11,12 +12,17 @@ namespace AbsenceManagement.Data.EF.Migrations
         }
 
         protected override void Seed(AbsenceManagementContext context) {
-            context.People.AddOrUpdate(
-                new PersonBuilder()
-                .CreatePerson("App", "Admin")
-                .WithDataSource("SYSTEM", "0")
-                .Build()
-            );
+            var systemUser =
+                context.People
+                    .FirstOrDefault(p => p.DataSource == "SYSTEM" && p.DataSourceId == "0");
+            if (systemUser != null) {
+                context.People.AddOrUpdate(
+                    new PersonBuilder()
+                    .CreatePerson("System", "User")
+                    .WithDataSource("SYSTEM", "0")
+                    .Build()
+                );
+            }
         }
     }
 }
