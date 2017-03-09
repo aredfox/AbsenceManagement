d672ccd97356b81ef15c809d97522e4aae451833
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-09 23:38:40 +0100

    Implemented the  "App" IUnitOfWork for EF.

---
+++ Source/Backend/Data/AbsenceManagement.Data.EF/App/EFUnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AbsenceManagement.Data.App;
+
+namespace AbsenceManagement.Data.EF.App
+{
+    public sealed class EFUnitOfWork : IUnitOfWork
+    {
+        private AbsenceManagementContext Database { get; }
+        public IExternalLoginRepository ExternalLoginRepository { get; }
+        public IRoleRepository RoleRepository { get; }
+        public IUserRepository UserRepository { get; }
+
+        public EFUnitOfWork(
+            AbsenceManagementContext dataContext,
+            IExternalLoginRepository externalLoginRepository,
+            IRoleRepository roleRepository,
+            IUserRepository userRepository
+        ) {
+            Database = dataContext;
+            ExternalLoginRepository = externalLoginRepository;
+            RoleRepository = roleRepository;
+            UserRepository = userRepository;
+        }
+
+        public int SaveChanges() {
+            return Database.SaveChanges();
+        }
+
+        public Task<int> SaveChangesAsync() {
+            return Database.SaveChangesAsync();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken) {
+            return Database.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
