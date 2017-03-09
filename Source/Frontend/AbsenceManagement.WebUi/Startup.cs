0e66d68cde1dd7a21151e068a122412412e6d21e
yves.schelpe@kdg.be <yves.schelpe@kdg.be>
2017-03-09 14:52:03 +0100

    Added WebUi project.

---
+++ Source/Frontend/AbsenceManagement.WebUi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AbsenceManagement.WebUi.Startup))]
+namespace AbsenceManagement.WebUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
