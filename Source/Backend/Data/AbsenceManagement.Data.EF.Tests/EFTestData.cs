e6ecf485993d49f5524b791b787f2d385d1e952c
yves.schelpe@kdg.be <yves.schelpe@kdg.be>
2017-03-09 13:27:58 +0100

    Keeping it dry in tests

--- Source/Backend/Data/AbsenceManagement.Data.EF.Tests/EFTestData.cs
+++ Source/Backend/Data/AbsenceManagement.Data.EF.Tests/EFTestData.cs
@@ -10,18 +10,41 @@ namespace AbsenceManagement.Data.EF.Tests
 {
     public static class EFTestData
     {
-        public static AbsenceManagementContext GetNewInMemoryAbsenceManagementContext(
-            IEnumerable<Person> people = null,
-            bool isTransient = true
+        public static AbsenceManagementContext GetTransientAbsenceManagementContext(
+            IEnumerable<Person> people = null        
         )
         {
-            var dbConnection = isTransient
-                ? Effort.DbConnectionFactory.CreateTransient()
-                : Effort.DbConnectionFactory.CreatePersistent("_");
+            var dbConnection = Effort.DbConnectionFactory.CreateTransient();
             var dbContext = new AbsenceManagementContext(dbConnection);
             dbContext.People.AddRange(people ?? new List<Person>());
             dbContext.SaveChanges();
             return dbContext;
-        }        
+        }
+
+        private static AbsenceManagementContext SetupPersistentAbsenceManagementContext(
+            string id = "_",
+            IEnumerable<Person> people = null            
+        )
+        {
+            var dbConnection = Effort.DbConnectionFactory.CreatePersistent(id);
+            var dbContext = new AbsenceManagementContext(dbConnection);
+            dbContext.People.AddRange(people ?? new List<Person>());
+            dbContext.SaveChanges();
+            return dbContext;
+        }
+
+        public static AbsenceManagementContext GetPersistentAbsenceManagentContext(
+            IEnumerable<Person> people = null
+        )
+        {
+            string id = DateTime.Now.Ticks.ToString();
+
+            using (var ctx = SetupPersistentAbsenceManagementContext(
+                id: id,
+                people: people ?? new Person[] { PersonBuilder.CreatePerson("John", "Doe").Build() })
+            ) { }
+
+            return SetupPersistentAbsenceManagementContext(id: id);
+        }
     }
 }
--- Source/Backend/Data/AbsenceManagement.Data.EF.Tests/People/EFDisconnectedPersonRepositoryTests.cs
+++ Source/Backend/Data/AbsenceManagement.Data.EF.Tests/People/EFDisconnectedPersonRepositoryTests.cs
@@ -17,7 +17,7 @@ namespace AbsenceManagement.Data.EF.Tests.People
         public void Can_Add() {
             // Arrange            
             var sut = new EFDisconnectedPersonRepository(
-                EFTestData.GetNewInMemoryAbsenceManagementContext()
+                EFTestData.GetTransientAbsenceManagementContext()
             );
             var expected = PersonBuilder.CreatePerson("John", "Doe").Build();
             // Act
@@ -33,16 +33,8 @@ namespace AbsenceManagement.Data.EF.Tests.People
         [Fact]
         public void Can_Update()
         {
-            // Arrange
-            using(var ctx = EFTestData.GetNewInMemoryAbsenceManagementContext(null, false)) {
-                var people = new Person[] {
-                    PersonBuilder.CreatePerson("John", "Doe").Build()
-                };
-                ctx.People.AddRange(people);
-                ctx.SaveChanges();
-            }
-
-            using(var ctx = EFTestData.GetNewInMemoryAbsenceManagementContext(null, false)) {
+            using(var ctx = EFTestData.GetPersistentAbsenceManagentContext()) {
+                // Arrange
                 var sut = new EFDisconnectedPersonRepository(ctx);
                 var johnId = sut.GetAll().FirstOrDefault().Id;
 
@@ -50,6 +42,7 @@ namespace AbsenceManagement.Data.EF.Tests.People
                 var actual = PersonBuilder.CreatePerson("Johnnie", "Doe").Build();
                 TestData.SetPrivateProperty(actual, nameof(Person.Id), johnId);
                 sut.Update(actual);
+
                 // Assert
                 Assert.Equal(1, sut.GetAll().Count());
                 Assert.NotNull(actual);
